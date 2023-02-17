6e3324f7697e605c8ab9df7d1bef840234a11841
MYTE <yasmi.tohabar@gmail.com>
2023-02-17 14:08:25 +0600

    user creation

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 from .serializers import RegisterSerializer
 from rest_framework.permissions import IsAdminUser, AllowAny
+from django.contrib.auth import authenticate
 
 
 class RegisterView(generics.CreateAPIView):
